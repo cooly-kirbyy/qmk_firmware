927415722d6ff3db1f753a716299e71631f5d367
Juda Ramsey <amanaramsey0128@gmail.com>
2026-09-19 03:28:59 +0000

    edited matrix, added encoder

--- keyboards/rhythmpad/keymaps/default/keymap.c
+++ keyboards/rhythmpad/keymaps/default/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │ G │ H │ I │
      * └───┴───┴───┘
      */
-    [0] = LAYOUT_ortho_3x3(
+    [0] = LAYOUT_ortho_3x4(
         KC_A,    KC_B,    KC_C,
         KC_D,    KC_E,    KC_F,
         KC_G,    KC_H,    KC_I
